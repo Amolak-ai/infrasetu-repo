e50ed3a531631314d2c30c754d6a903c11e9b509
Amolak-ai <amolvakotkar@gmail.com>
2025-07-21 22:59:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,49 @@
-resource "azurerm_resource_group" "rg1" {
-    name     = "rgsetu01"
+resource "azurerm_resource_group" "rg01" {
+    name     = "rgsetu0001"
     location = "West Europe"
   
 }
 
-resource "azurerm_resource_group" "rg2" {
-    name     = "rgsetu02"
+resource "azurerm_resource_group" "rg02" {
+    name     = "rgsetu0002"
     location = "West Europe"
   
 }
 
-resource "azurerm_storage_account" "storage1" {
-    name                     = "setustg1"
+resource "azurerm_storage_account" "storage01" {
+    name                     = "setustg11"
     resource_group_name      = azurerm_resource_group.rg1.name
     location                 = azurerm_resource_group.rg1.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account" "storage2" {
-    name                     = "setustg2"
+resource "azurerm_storage_account" "storage02" {
+    name                     = "setustg12"
     resource_group_name      = azurerm_resource_group.rg2.name
     location                 = azurerm_resource_group.rg2.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_account" "storage3" {
-    name                     = "setustg3"
+resource "azurerm_storage_account" "storage03" {
+    name                     = "setustg13"
     resource_group_name      = azurerm_resource_group.rg2.name
     location                 = azurerm_resource_group.rg2.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "storage04" {
+    name                     = "setustg14"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+
+
+
 
 resource "azurerm_storage_account" "storage4" {
     name                     = "setustg4"
